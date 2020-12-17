0e705166afaaca218691d1fb3fba382e9eac81b1
Ana Calalb <calalb.ana@gmail.com>
2020-12-17 18:25:39 +0200

    Lab11

--- Calalb_Ana_Lab10/ListEntryPage.xaml.cs
+++ Calalb_Ana_Lab10/ListEntryPage.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Calalb_Ana_Lab10.Models;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
